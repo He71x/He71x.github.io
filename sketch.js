c57000613c23b2418cb1d3e3cb5b9cd5d581a155
He71x <he71x123@gmail.com>
2026-02-09 12:18:16 -0600

    1
    1

---
+++ sketch.js
@@ -0,0 +1,28 @@
+//Basics pf Coordinnate systems
+//IN PYTHON, WE WROTE "RUN TO COMPLETION"
+// (start at top, end at bottom)
+
+//IN p5.js, we write "interactive"
+// setup() - runs once, at start
+// draw() - runs over and over(after setup)
+// targeting 60 times per second  
+//SCREEN IS UPDATED AT BOTTOM OF DRAW
+//
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(0); //WIPES THE SCREEN}
+
+
+//     x    y   dia
+
+
+circle(0, 0, 50) ;
+}
+
+
+
