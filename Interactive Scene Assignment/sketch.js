f6c60538486232b70deb8cc6d972bc6ddc6848cf
He71x <he71x123@gmail.com>
2026-02-12 11:24:53 -0600

    INTERACTING SCENE

---
+++ Interactive Scene Assignment/sketch.js
@@ -0,0 +1,38 @@
+// Interactive Scene Assignment
+// Amaan Rehman
+// 2/11/26
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+}
+
+function draw() {
+  background(bgcolor);
+  
+
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
