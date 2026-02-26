638a08e015a5b1308aed75dbd932706e13e6b3e2
He71x <he71x123@gmail.com>
2026-02-25 18:40:43 -0600

    part 7 interactive

--- Interactive Scene Assignment/sketch.js
+++ Interactive Scene Assignment/sketch.js
@@ -7,15 +7,50 @@
 
  function setup() { 
   createCanvas(windowWidth, windowHeight); }
+
+
+  let r = 10;
+  let g = 196;
+  let b = 224;
+
+
+  let changedColor = true;
+
+  let sunColor = "yellow";
+
  
+
+
+  let value = 0;
+
  //Create functions for objects and background function 
  
  function grass(){ 
-  noStroke(); 
-  fill("Green"); 
+ noStroke();
+  if(value === 0){
+    fill("green");
+  } else{
+      fill("brown");
+
+
   rect(0, 700, 950, 400);
  }
+ }
 
+ function mouseClicked() {
+if(value === 0){
+value = 255;
+}else {
+  value = 0
+
+
+}
+
+
+}
+
+
+ 
  function colors1() { 
   noStroke();
    fill("navy"); 
@@ -45,7 +80,7 @@ function colors3() {
 
    function sun() {
       noStroke();
-      fill("yellow")
+      fill(sunColor)
       circle(50, 50, 250)
 
    }
@@ -65,9 +100,6 @@ fill("yellow")
 circle(mouseX + 20, mouseY + 29, 25)
 circle(mouseX + 45, mouseY + 20, 25)
 
-circle(mouseX + 20, mouseY + 20, 25)
-circle(mouseX + 90, mouseY + 20, 25)
-
 
 }
 
@@ -77,11 +109,41 @@ function object() {
   rect(15, 700, 55, -230);
   rect(880, 700, 55, -230)
 
+  
+
 }
+//Swtitch between pressing s and another key, for example like s and d repeatedly.
+function keyPressed() {
+if(key === 's'){
+    if(changedColor === false) {
+
+      sunColor = "orange";
+      changedColor = true;
+
+}
+    else{
+      sunColor = "yellow"
+      changedColor = false
+
+    }
+    }
+
+
+
+
+
+
+
+
+
+}
+
+
+
 
  
  function draw() { 
-  background(10, 196, 224);
+  background(r, g, b);
    grass(); 
    colors1();
     colors2(); 
@@ -91,7 +153,7 @@ function object() {
     word();
     object();
     character();
-    
+    keyPressed();
   }
 
   
