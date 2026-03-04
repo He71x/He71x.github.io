fcd49f3b1da727153da5ba4cdec7eb4d5b111558
He71x <he71x123@gmail.com>
2026-03-04 12:18:36 -0600

    Perlin noise 2

--- Perlin Noise Assignment/sketch.js
+++ Perlin Noise Assignment/sketch.js
@@ -1,15 +1,52 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Perlin Noise Project
+// Amaan Rehman
+// 3/4/26
+
+//Make terrains
+
+
+let rectWidth = 15;
+
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  fill(100);
 }
 
+function generateTerrain() {
+  //USE A LOOP TO PUT A NUMBER
+  //Side of side rectangles
+  //random height
+  for(let x = 0; x < width; x+= rectWidth){
+    //random height
+    let rectHeight = random(0, height * 0.73);
+
+    rect(x, height, rectWidth, -rectHeight);
+  }
+
+
+
+}
+
+
+
+
 function draw() {
-  background(220);
+  //background(220);
+  //generateTerrain();
+  //frameRate(10);
+
+  let noiseLevel = 1000;
+  let noiseScale = 0.02;
+
+
+  let x = frameCount;
+  let nx = noiseScale * x;
+
+  let y = noiseLevel * noise(nx);
+
+  line(x, y, x ,height);
+
+  
 }
