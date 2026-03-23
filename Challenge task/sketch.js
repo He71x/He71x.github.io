42572bc869bc47b14fb590cf8f942a1687017738
He71x <he71x123@gmail.com>
2026-03-23 12:17:32 -0600

    CHALLENGE

---
+++ Challenge task/sketch.js
@@ -0,0 +1,33 @@
+// Challenge Task
+// Amaan Rehman
+// 3/23/26
+//
+
+let items = [];
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  for(let i = 0; i < 20; i++){
+    items.push;
+    x = random(60, width - 60);
+    y = random(height);
+    speedY = random(1, 3);
+    basePrice = random(10, 100);
+
+    name = "item" + i
+
+
+    
+
+
+
+  }
+}
+
+
+
+
+function draw() {
+  background(220);
+}
