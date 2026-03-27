44d4baee2700faa5fd4c1fe89f849ac4625fcdd5
He71x <he71x123@gmail.com>
2026-03-27 12:17:28 -0600

    cars part 2

--- Cars Cars Cars! Assignment/sketch.js
+++ Cars Cars Cars! Assignment/sketch.js
@@ -10,22 +10,31 @@ let westbound = [];
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
-  for(let i = 0; i < 20; i++){
-    let y = random(height/2, height-40);
-    eastbound.push(new Vehicle())
-  }
 
-}
+//   for(let i = 0; i < 20; i++){
+//     let x = random(height/2, height - 40);
+//     eastbound.push(new Vehicle(1, random(width)));
+//  }
+
+ }
   
 function drawRoad(){
+fill(0);
+rect(0, 330, 1000, 300);
+stroke(255);
 
+strokeWeight(4);
+
+for(let x = 0; x < width; x += 45){
+  line(x, width/2, x + 21, width/2)
+}
 }
 
 
 
 function draw() {
-  background(220);
-  drawRoad()
+  background(250);
+  drawRoad();
 }
 
 
