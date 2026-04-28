67b943006d13a0961a4a5b1ac89f429befac76de
He71x <he71x123@gmail.com>
2026-04-28 11:16:59 -0600

    part 2 image manip

--- Image exercise first one/sketch.js
+++ Image exercise first one/sketch.js
@@ -6,14 +6,11 @@
 
 let myImage;
 
-
-
-
-function preLoad(){
+function preload(){
   myImage = loadImage("assets/chip.jpg");
 }
 function setup() {
-  createCanvas(myImage.width, myImage. height);
+  createCanvas(640, 480);
   pixelDensity(1);
 }
 
@@ -28,5 +25,43 @@ function draw() {
 
 
 function majorityColor(){
-  
+  //check every pixel then change
+  for(let x = 0; x < width; x++){
+     for(let y = 0; y < height; y++){
+
+      let index = ((y * width) + x) * 4;
+
+      let r = pixels[index];
+      let g = pixels[index+1];
+      let b = pixels[index + 2];
+
+      if(r >= g && r>= b){
+        setPixel(x,y,255,0,0);
+        //go pure red
+      }
+      else if(g>=b){
+        setPixel(x,y,0,255,0);
+        //green
+      }
+      else{
+        setPixel(x,y,0,0,255);
+        //else blue to be biggest
+      }
+     }
+  }
+
+}
+
+function setPixel(x,y,r,g,b){
+
+  let index = ((y*width) + x)*4;
+  setPixelOneD(index,r,g,b);
+}
+
+function setPixelOneD(pos, r, g, b){
+
+  pixels[pos] = r;
+  pixels[pos+1] = g;
+  pixels[pos+2] = b;
+
 }
