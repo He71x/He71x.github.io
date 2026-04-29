28cecb73a938dd27e6f536b357bdb06d4ecccbe3
He71x <he71x123@gmail.com>
2026-04-29 11:07:47 -0600

    right green side whole assignment

---
+++ No Green right side 2nd exercise/sketch.js
@@ -0,0 +1,54 @@
+// Secone image exercise
+// Amaan Rehman
+// 4/28/26
+//
+
+let myImage;
+
+
+function preload(){
+  myImage = loadImage("assets/race.jpg");
+}
+
+function setup() {
+  createCanvas(myImage.width, myImage.height);
+  pixelDensity(1);
+}
+
+function draw() {
+  background(220);
+  image(myImage, 0, 0);
+  loadPixels();
+  rightSide();
+  updatePixels();
+}
+
+
+function rightSide(){
+  for(let x = 0; x < width; x++){
+    for(let y = 0; y < height; y++){
+      let index = ((y*width) + x)*4;
+
+    let r = pixels[index];
+    let g = pixels[index+1];
+    let b = pixels[index+2];
+
+    if(x > width / 2){
+      setPixel(x,y,r, 0, b);
+    }
+    }
+    
+    }
+  }
+  function setPixel(x,y,r,g,b){
+
+  let index = ((y*width) + x)*4;
+  setPixelOneD(index,r,g,b);
+}
+
+function setPixelOneD(pos, r, g, b){
+  
+  pixels[pos] = r;
+  pixels[pos+1] = g;
+  pixels[pos+2] = b;
+}
