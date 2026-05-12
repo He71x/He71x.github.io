e9be47b8daecda3516a8f06e5f50d585f9d66e4f
He71x <he71x123@gmail.com>
2026-05-12 12:19:05 -0600

    part 5 capstone
    added the menu background image, adjusted the size and for the canvas too. The title also put in its respective place, changed font. Added some button functions too, to be continued.

--- Capstone Coding Project Obstacle course/sketch.js
+++ Capstone Coding Project Obstacle course/sketch.js
@@ -19,15 +19,26 @@ let optionsButton;
 
 function preload(){
   //loads menu background image
-  //menuBg = loadImage("");
+  menuBg = loadImage("assets/Menu Background.jpeg");
 
   //loads playbutton image
   //playButton = loadImage("");
 
+  //loads optionsButton image
+  //optionsButton = loadImage("");
+
+  //loads zombie image
+  //zombieImg = loadImage("");
+
+  //loads character image
+  //characterImg = loadImage("");
+
+
+
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(950, 980);
 
 }
 
@@ -44,29 +55,41 @@ function draw() {
     level1();
 
   }
-  
-
-  
 }
 
 function menuScreen(){
-  background(20);
-  //images not yet added
-//image(menuBg, 0,0,width,height);
+image(menuBg, 0,0,width,height);
+//image(playButton,0,500,width/2,height/2);
 
-fill(0,150);
-rect(0,0, width, height);
+//positions for the "play" button
+let buttonX = width/2;
+let buttonY = height/2;
+
+let buttonSize1 = 250
+let buttonSize2 = 100
+
+
+
+
+
+
+
+
+
+let optionX = width/2;
+let optionY = height/2;
+
+let optionSize1 = 300
+let optionSize2 = 200
+
+
 fill("red");
-
 textAlign(CENTER);
-textFont('Courier New');
+textFont('bold');
 
 textSize(90);
-text("THE DEATH CHASE", width/2, 700);
+text("THE DEATH CHASE", width/2, 100);
 }
-
-
-
 
 
 function optionsScreen(){
