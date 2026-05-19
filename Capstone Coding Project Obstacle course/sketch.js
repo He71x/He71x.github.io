46270f26ebf46fd8ad2c72916974fc6af47360db
He71x <he71x123@gmail.com>
2026-05-19 11:19:49 -0600

    part 9 capstone
    changed the button layout a little so when i move from menu to level one, it disappears. Used class to compliment the platform but still figuring out if a shape could touch it and added a testing shape to test if the platform would work later.

--- Capstone Coding Project Obstacle course/sketch.js
+++ Capstone Coding Project Obstacle course/sketch.js
@@ -12,7 +12,14 @@ let menuBg;
  let levelBg1;
  let characterImg;
  let zombieImg;
+ let button;
 
+ let platforms = [];
+//prototype player visibility
+ let playerX = 10;
+ let playerY = 700;
+
+ let playerSize = 50;
 
 function preload(){
   //loads menu background image
@@ -31,11 +38,14 @@ function preload(){
 
 function setup() {
   createCanvas(950, 980);
-  let button =  createButton("PLAY");
+ button =  createButton("PLAY");
 button.position(385,755);
 button.size(200, 50);
 button.mousePressed(repaint);
-button.mousePressed(swapBg);
+button.mousePressed(startGame);
+
+//places platform in its spot and used array
+platforms.push(new Platform(0,850,970,130));
 
 }
 
@@ -48,22 +58,64 @@ function draw() {
   textFont('bold');
   textSize(90);
   text("THE DEATH CHASE", width/2, 100);
-
  }
  else{
 scale(1, 0.7);
   
   image(levelBg1,0, 0, width, height);
-  rect(0,1000,500, 900);
+  fill("darkred")
+  rect(0,975, 1000, 900);
 
+
+  //display the platform but not shown
+  for(let i of platforms){
+    i.display();
+  }
+
+  fill("blue")
+  rect(playerX, playerY, playerSize);
+
+  //movement for prototype player
+  if(keyIsDown(LEFT_ARROW)){
+    playerX -= 5;
+  }
+  else{
+    if(keyIsDown(RIGHT_ARROW)){
+      playerX += 5;
+    }
+  }
  
  }
   }
-function swapBg(){
-  firstImage =! firstImage;
+function startGame(){
+  //switches screen and button hides after its in level 1
+  firstImage = !firstImage;
+  button.hide();
 }
+
+//
 function repaint(){
-  let g = 255;
+  //highlights the button when hovering over
+  let g = 5000;
   background(g);
+}
+
+//use classes for platforms
+class Platform{
+  constructor(x,y, w, h){
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+}
+display(){
+  //shows image backrgound only and platoform rect shape not shown.
+  noFill();
+  noStroke();
+
+  rect(this.x,this.y,this.w,this.h);
+ 
+}
+
 }
 
