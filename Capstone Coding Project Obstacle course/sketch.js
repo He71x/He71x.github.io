154fc7652d9e5bac8e188340d857ae5591ec468f
He71x <he71x123@gmail.com>
2026-06-03 12:17:20 -0600

    part 22 capstone
    jumping has been added and works but physics seem wonky since player can just run while jumping and will already be at the end. Looking for the player to not be able to run while in the air with one more level left

--- Capstone Coding Project Obstacle course/sketch.js
+++ Capstone Coding Project Obstacle course/sketch.js
@@ -13,36 +13,37 @@ let levelBg1;
 let characterImg;
 let zombieImg;
 let button;
+
 let runImg;
 let levelBg2;
-
+let jumpImg;
 let frameX = 0;
 let frameY = 0;
 
 let idleFrameX = 0;
 let idleFrameY = 0;
 
- //this stops the player from exiting screen
+//this stops the player from exiting screen
 let barriers = [];
 let level = 1;
 
 //the spikes on the screen which restarts the player
 //to the start
- let spikes = [];
+let spikes = [];
 
 
- let spriteW = 260;
- let spriteH = 300;
+let spriteW = 260;
+let spriteH = 300;
 
 
- let idleSpriteW = 260;
- let idleSpriteH = 300;
+let idleSpriteW = 260;
+let idleSpriteH = 300;
 
- let player;
+let player;
 
- let platforms = [];
+let platforms = [];
 
-function preload(){
+function preload() {
   //this loads menu background image
   menuBg = loadImage("assets/Menu Background.jpeg");
 
@@ -56,261 +57,270 @@ function preload(){
   //zombieImg = loadImage("");
 
   //this loads character + idle image
- characterImg = loadImage("assets/idleAnim.png");
+  characterImg = loadImage("assets/idleAnim.png");
 
- //this loads running ani image
- runImg = loadImage("assets/runAni.png");
+  //this loads running ani image
+  runImg = loadImage("assets/runAni.png");
+
+  //loads jump image
+  //jumpImg = loadImage("assets/");
 
 }
 
 function setup() {
   createCanvas(950, 980);
- button =  createButton("PLAY");
-button.position(385,755);
-button.size(200, 50);
-button.mousePressed(repaint);
-button.mousePressed(startGame);
+  button = createButton("PLAY");
+  button.position(385, 755);
+  button.size(200, 50);
+  button.mousePressed(repaint);
+  button.mousePressed(startGame);
 
-player = new Player(-30,130);
+  player = new Player(-30, 130);
 
-//places platform in its spot and used arrays
-//floor
-platforms.push(new Platform(0,990,970,130));
-//2nd plat
-platforms.push(new Platform(175,529,10,100));
-//1st plat 
-platforms.push(new Platform(20,323,10,100));
+  //places platform in its spot and used arrays
+  //floor
+  platforms.push(new Platform(0, 990, 970, 130));
+  //2nd plat
+  platforms.push(new Platform(175, 529, 10, 100));
+  //1st plat 
+  platforms.push(new Platform(20, 323, 10, 100));
 
-platforms.push(new Platform(375,380,-50,100));
-platforms.push(new Platform(510,570,160,100));
-platforms.push(new Platform(900,425,10,100));
+  platforms.push(new Platform(375, 380, -50, 100));
+  platforms.push(new Platform(510, 570, 160, 100));
+  platforms.push(new Platform(900, 460, 10, 100));
 
-//places spikes and restarts player back to the start if touched
-spikes.push(new Spike(460, 550, -45, 80));
-spikes.push(new Spike(690, 550, -55, 80));
+  //places spikes and restarts player back to the start if touched
+  spikes.push(new Spike(500, 550, -110, 80));
+  spikes.push(new Spike(690, 550, -85, 80));
 
-barriers.push(new Barrier(-120, 0, 10, height));
-barriers.push(new Barrier(1000, 570, 10, 400));
+  barriers.push(new Barrier(-120, 0, 10, height));
+  barriers.push(new Barrier(1000, 570, 10, 400));
 
 }
 function draw() {
   background(220);
- if(firstImage){
-  image(menuBg, 0,0,width,height); 
-  fill("red");
-  textAlign(CENTER);
-  textFont('bold');
-  textSize(90);
-  text("THE DEATH CHASE", width/2, 100);
- }
- else{
-scale(1, 0.7);
-  
-  image(levelBg1,0, 0, width, height);
-  
-  fill("darkred")
-  rect(0,975, 1000, 900);
-
-
-  //display the platform but not shown
-  for(let p of platforms){
-  
-    p.display();
+  if (firstImage) {
+    image(menuBg, 0, 0, width, height);
+    fill("red");
+    textAlign(CENTER);
+    textFont('bold');
+    textSize(90);
+    text("THE DEATH CHASE", width / 2, 100);
   }
+  else {
+    scale(1, 0.7);
 
-  for(let s of spikes){
+    image(levelBg1, 0, 0, width, height);
 
-    s.display();
-    if(player.pos.x + player.hitboxW > s.x &&
-      player.pos.x < s.x + s.w &&
-      player.pos.y + player.hitboxH > s.y &&
-      player.pos.y < s.y + s.h){
-    
-    player.pos.x = -90;
-    player.pos.y = 100;
-    player.vel.y = 0;
+    fill("darkred")
+    rect(0, 975, 1000, 900);
 
-      }
-  }
 
-  player.move();
-  
+    //display the platform but not shown
+    for (let p of platforms) {
 
-   for(let b of barriers){
-    b.display();
-    
-
-    if(player.pos.x + player.hitboxW > b.x &&
-       player.pos.x < b.x + b.w &&
-       player.pos.y + player.hitboxH > b.y &&
-       player.pos.y < b.y + b.h){
-       
-
-       
-      if(b.x < width/2){
-        player.pos.x = b.x + b.w;
-      }
-      else{
-        player.pos.x = b.x - player.hitboxW;
-      }
-        
-  }
-   }
-   player.display();
- 
-}
-}
- //add class for the player for movement and physics
- //when touching ground or platforms shown
-  class Player{
-    constructor(x,y){
-      //using inspiration from the vectors exercise
-      this.pos = createVector(x,y);
-      this.vel = createVector(0,0);
-      this.force = createVector(0, 0.05);
-      this.imgW = 250;
-      this.imgH = 300;
-
-      this.hitboxW = 190;
-      this.hitboxH = 210;
-    
+      p.display();
     }
-    move(){
+
+    for (let s of spikes) {
+
+      s.display();
+      if (player.pos.x + player.hitboxW > s.x &&
+        player.pos.x < s.x + s.w &&
+        player.pos.y + player.hitboxH > s.y &&
+        player.pos.y < s.y + s.h) {
+
+        player.pos.x = -90;
+        player.pos.y = 100;
+        player.vel.y = 0;
+
+      }
+    }
+
+    player.move();
+
+
+    for (let b of barriers) {
+      b.display();
+
+
+
+      if (player.pos.x + player.hitboxW > b.x &&
+        player.pos.x < b.x + b.w &&
+        player.pos.y + player.hitboxH > b.y &&
+        player.pos.y < b.y + b.h) {
+
+        if (b.x < width / 2) {
+          player.pos.x = b.x + b.w;
+        }
+        else {
+          player.pos.x = b.x - player.hitboxW;
+        }
+
+      }
+    }
+    player.display();
+
+  }
+}
+//add class for the player for movement and physics
+//when touching ground or platforms shown
+class Player {
+  constructor(x, y) {
+    //using inspiration from the vectors exercise
+    this.jumping = false;
+    this.pos = createVector(x, y);
+    this.vel = createVector(0, 0);
+    this.force = createVector(0, 0.05);
+    this.imgW = 250;
+    this.imgH = 300;
+
+    this.hitboxW = 190;
+    this.hitboxH = 210;
+
+  }
+  move() {
+
+    if (keyIsDown(32) && this.jumping === false) {
+      this.vel.y -= 10;
+      this.jumping = true;
+    }
     //adds the gravity
-      this.vel.add(this.force);
-      this.pos.add(this.vel);
+    this.vel.add(this.force);
+    this.pos.add(this.vel);
 
-      if(keyIsDown(65)){
-        this.pos.x -= 6;
-//runs every 6 frames to slow down animation
-        if(frameCount % 6 === 0){
-          //moves on to next frame animation
-          frameX ++;
+    if (keyIsDown(65)) {
+      this.pos.x -= 5;
+      //runs every 6 frames to slow down animation
+      if (frameCount % 6 === 0) {
+        //moves on to next frame animation
+        frameX++;
 
-          if(frameX > 4){
-              frameX = 0;
-              //loops animation back to start in a loop
-          }
-        }
-    }
-    if(keyIsDown(68)){
-      this.pos.x += 6;
-      if(frameCount % 6 === 0){
-          frameX ++;
-
-          if(frameX > 4){
-              frameX = 0;
-          }
+        if (frameX > 4) {
+          frameX = 0;
+          //loops animation back to start in a loop
         }
       }
-  
-  for(let p of platforms){
-if(this.pos.x + this.hitboxW > p.x &&
-this.pos.x < p.x + p.w &&
-this.pos.y + this.hitboxH > p.y &&
-this.pos.y + this.hitboxH < p.y + p.h){
+    }
+    if (keyIsDown(68)) {
+      this.pos.x += 5;
+      if (frameCount % 6 === 0) {
+        frameX++;
 
-  this.pos.y = p.y - this.hitboxH;
-  this.vel.y = 6;
-}
+        if (frameX > 4) {
+          frameX = 0;
+        }
+      }
+    }
+
+    for (let p of platforms) {
+      if (this.pos.x + this.hitboxW > p.x &&
+        this.pos.x < p.x + p.w &&
+        this.pos.y + this.hitboxH > p.y &&
+        this.pos.y + this.hitboxH < p.y + p.h) {
+
+        this.pos.y = p.y - this.hitboxH;
+        this.vel.y = 8;
+        this.jumping = false;
+      }
+    }
   }
-}
-display(){
-  if(keyIsDown(65)){
-    push();
-    //flips image
-    scale(-1,1);
-    //shows x and y position of the player, 
-    // width and height of image and selects each 
-    // animation from each frame. Minusing the positionx with
-    //image width to change directions when pressing d 
-    //to turn left
-    image(runImg, -this.pos.x - this.imgW, this.pos.y, 
-      this.imgW, this.imgH, frameX * spriteW, frameY * spriteH,
-      spriteW, spriteH);
+  display() {
+    if (keyIsDown(65)) {
+      push();
+      //flips image
+      scale(-1, 1);
+      //shows x and y position of the player, 
+      // width and height of image and selects each 
+      // animation from each frame. Minusing the positionx with
+      //image width to change directions when pressing d 
+      //to turn left
+      image(runImg, -this.pos.x - this.imgW, this.pos.y,
+        this.imgW, this.imgH, frameX * spriteW, frameY * 
+        spriteH, spriteW, spriteH);
       pop();
-  }
-  else if(keyIsDown(68)){
-    image(runImg, this.pos.x, this.pos.y, 
-      this.imgW, this.imgH, frameX * spriteW, 
-      frameY * spriteH,
-      spriteW, spriteH);
-  }
-  else{
-    //idle animation start similar to running sprite sheet
-    if(frameCount % 20 === 0){
-      frameX++;
-      
-      if(frameX > 3){
-        frameX = 0;
+    }
+    else if (keyIsDown(68)) {
+      image(runImg, this.pos.x, this.pos.y,
+        this.imgW, this.imgH, frameX * spriteW,
+        frameY * spriteH,
+        spriteW, spriteH);
+    }
+    else {
+      //idle animation start similar to running sprite sheet
+      if (frameCount % 20 === 0) {
+        frameX++;
+
+        if (frameX > 3) {
+          frameX = 0;
+        }
       }
-    }
-    image(characterImg, this.pos.x, this.pos.y, this.imgH,
-          this.imgW, idleFrameX * idleSpriteW, 0, 
-          idleSpriteW, idleSpriteH);
-    }
+      image(characterImg, this.pos.x, this.pos.y, this.imgH,
+        this.imgW, idleFrameX * idleSpriteW, 0,
+        idleSpriteW, idleSpriteH);
     }
   }
+}
 //use classes for platforms
-class Platform{
-  constructor(x,y, w, h){
-    this.x = x;
-    this.y = y;
-    this.w = w;
-    this.h = h;
-}
-display(){
-  //shows image backrgound only and platoform rect shape not shown.
-          noFill();
-          noStroke();
-
-  rect(this.x,this.y,this.w,this.h);
-
-}
-}
-
-class Spike{
-  constructor(x, y, w, h){
+class Platform {
+  constructor(x, y, w, h) {
     this.x = x;
     this.y = y;
     this.w = w;
     this.h = h;
   }
-  display(){
+  display() {
+    //shows image backrgound only and platoform rect shape not shown.
+    noFill();
+    noStroke();
 
-    
+    rect(this.x, this.y, this.w, this.h);
+
+  }
+}
+
+class Spike {
+  constructor(x, y, w, h) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+  }
+  display() {
+
+
     //triangle shape
-    triangle(this.x, this.y + this.h, this.x + this.w/2, 
+    triangle(this.x, this.y + this.h, this.x + this.w / 2,
       this.y, this.x + this.w, this.y + this.h);
-}
+  }
 }
 
-class Barrier{
-  constructor(x,y,w,h){
+class Barrier {
+  constructor(x, y, w, h) {
     this.x = x;
     this.y = y;
     this.w = w;
     this.h = h;
   }
-  display(){
+  display() {
 
     fill("blue")
-    rect(this.x,this.y,this.w,this.h);
+    rect(this.x, this.y, this.w, this.h);
   }
 }
 
-function startGame(){
+function startGame() {
   //switches screen and button hides after its in level 1
   firstImage = !firstImage;
   button.hide();
 }
 
 //
-function repaint(){
+function repaint() {
   //highlights the button when hovering over
   let g = 5000;
   background(g);
 }
 
-//function 
+//function transistion
 
